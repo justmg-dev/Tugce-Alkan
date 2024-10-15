6478bd163c10e3f511d4ecb7cfa1cc3c68e52204
itsmgmate <muhammetgulogluu@gmail.com>
2024-10-15 21:15:15 +0300

    initial

---
+++ hakkimda.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hakkımda | Psikolog Tuğçe Alkan</title>
+    <meta name="description" content="Ergen &amp; Yetişkin Psikoterapisti">
+    <link rel="stylesheet" href="/css/stylehakkimda.css">
+    <script src="https://kit.fontawesome.com/8af256a44f.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="/images/mainlogo.png" type="image/x-icon">
+</head>
+
+<body>
+    <nav>
+        <div class="navbar">
+            <div class="container nav-container">
+                <input class="checkbox" type="checkbox" name="" id="" />
+                <div class="hamburger-lines">
+                    <span class="line line1"></span>
+                    <span class="line line2"></span>
+                    <span class="line line3"></span>
+                </div>
+                <div class="logomobile">
+                    <img src="/images/mainlogo.png" alt="tugcealkanlogo" srcset="">
+                    <div>
+                        <h2>Psikolog</h2>
+                        <h1>Tuğçe Alkan</h1>
+                    </div>
+                </div>
+                <div class="menu-items">
+                    <li><a href="/index.php">ANA SAYFA</a></li>
+                    <li><a href="/hakkimda.php">HAKKIMDA</a></li>
+                    <li><a href="/calismalarim.php">ÇALIŞMA ALANLARI</a></li>
+                    <li><a href="https://www.tugcealkan.com/blog">BLOG</a></li>
+                    <li><a href="/iletisim.php">RANDEVU &AMP; İLETİŞİM</a></li>
+                    <img src="/images/mainlogo.png" alt="tugcealkanlogo" srcset="">
+                    <h2>Psikolog</h2>
+                    <h1>Tuğçe Alkan</h1>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="headercontainer">
+        <div class="header">
+            <div class="logowebpage"><a href="/index.php">
+                    <img src="/images/mainlogo.png" alt="tugcealkanlogo" srcset="">
+                    <div class="logocaption1">
+                        <h2>Psikolog</h2>
+                        <h1>Tuğçe Alkan</h1>
+                    </div>
+                </a>
+            </div>
+
+            <div class="navbars">
+                <ul class="menulist">
+                    <li class="menu"><i class="fa-brands fa-pagelines"></i>&nbsp;<a href="/hakkimda.php">HAKKIMDA</a>
+                    </li>
+                    <li class="menu"><a href="/calismalarim.php" id="redirectButton">ÇALIŞMA ALANLARI</a></li>
+                    <li class="menu"><a href="/iletisim.php">İLETİŞİM</a></li>
+                    <li class="menu"><a href="https://www.tugcealkan.com/blog/">BLOG</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="introduction">
+        <div class="introductioncontainer">
+            <div class="biography">
+                <div class="biocontainer">
+                    <div class="headercaption">
+                        <h3>Psikolog Tugce Alkan</h3>
+                    </div>
+                    <div class="bioinfo1">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis dolorem nisi vel
+                            praesentium
+                            unde
+                            modi inventore molestiae reprehenderit ullam nulla rem sunt blanditiis est id, mollitia
+                            asperiores
+                            totam quam perferendis.
+                        </p>
+                    </div>
+                    <div class="bioinfo2">
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa iste totam adipisci esse
+                            repellat
+                            porro ut vitae sit! Deleniti, neque? Enim autem numquam distinctio a doloremque
+                            necessitatibus
+                            dolorum voluptatem nihil.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="introductionphoto">
+                <div class="photocontainer">
+                    <img src="/images/foto2.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="navlinkscontainer">
+            <div class="footerlogo1">
+                <a href="">
+                    <img src="/images/mainlogo.png" alt="Psikolog Tuğçe Alkan, Ergen Psikoterapist, Yetişkin Psikoterapist" srcset="">
+                    <div class="logocaption2">
+                        <h2>Psikolog</h2>
+                        <h1>Tuğçe Alkan</h1>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="bookingbutton"></div>
+
+        <div class="footerinfo">
+            <span class="footerintro"><a href="https://wa.me/+905306137411?text=Doğum%20Günüskon%20KUTLU%20OLSUUuUuunn" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></span>
+            <span class="footerintro"><a href="https://maps.app.goo.gl/uXh8HuBm65SQh7rn7" target="_blank"><i class="fa-solid fa-map-location-dot"></i></a></span>
+            <span class="footerintro"><a href="tel:+905306137411"><i class="fa-solid fa-phone"></i></a></span>
+            <span class="footerintro"><a href="https://www.instagram.com/psikologtugcealkan" target="_blank"><i class="fa-brands fa-instagram"></i></a></span>
+        </div>
+
+        <div class="footernavbar">
+            <ul class="footermenulist">
+                <li><a href="/index.php">ANA SAYFA</a></li>
+                <li><a href="/hakkimda.php">HAKKIMDA</a></li>
+                <li><a href="/calismalarim.php">ÇALIŞMA ALANLARI</a></li>
+                <li><a href="https://www.tugcealkan.com/blog">BLOG</a></li>
+            </ul>
+        </div>
+
+        <a href=""><span class="attribute">mg.dev</span></a>
+    </div>
+</body>
+
+</html>
